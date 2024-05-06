53406406d2fcc2cb896da5925c1a01739d6308c2
HOA2205 <phatnguyentien2311@gmail.com>
2024-05-06 15:55:34 +0700

    thanhnam

---
+++ obf_C79Vn0ebURGr90Xep8py70yLRcPADF6p9ZrKMc4fZa9a0y86cjBtVF4soin95rkC.lua
@@ -0,0 +1,12 @@
+UDim2.new(1,0,1,0)
+                        name.TextYAlignment = 'Top'
+                        name.BackgroundTransparency = 1
+                        name.TextStrokeTransparency = 0.5
+                        name.TextColor3 = Color3.fromRGB(7, 236, 240)
+                    else
+                        v['NameEsp'].TextLabel.Text = (v.Name ..'   \n'.. round((game:GetService('Players').LocalPlayer.Character.Head.Position - v.Position).Magnitude/3) ..' Distance')
+                    end
+                end
+            else
+                if v:FindFirstChild('NameEsp') then
+                    v:FindFirstChild('NameEsp'
